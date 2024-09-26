37d92c59a82b13199d05eb7113ea07d0566a9271
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-09-27 00:11:57 +0200

    Tree class

--- tree_traversal.py
+++ tree_traversal.py
@@ -4,3 +4,20 @@ class Node:
 
     def set_value(self, new_value: int) -> None:
         self.value = new_value
+
+class Tree:
+    def __init__(self, root: Node, right:"Tree" = None, left:"Tree" = None) -> None:
+        self.root = root
+        self.right = right
+        self.left = left
+
+    def get_root(self) -> int:
+        return self.root.value
+    
+    def set_left(self, left_tree: "Tree") -> "Tree":
+        self.left = left_tree
+        return self
+        
+    def set_right(self, right_tree: "Tree") -> "Tree":
+        self.right = right_tree
+        return self
